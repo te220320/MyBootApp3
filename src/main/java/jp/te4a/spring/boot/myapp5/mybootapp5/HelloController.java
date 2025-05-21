ee8add249b69e58bf379a491bc6a6557008d1422
Fujiwara <te220320@gmail.com>
2025-05-21 12:25:08 +0900

    kadai5のコミット

---
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -0,0 +1,28 @@
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg","this is a settig message");
+        //return "this is Spring Boot sample."
+    return "index";
+    }
+    @RequestMapping(value="post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
+    }
+    
+}
--- src/test/java/com/example/mybootapp3/Mybootapp3ApplicationTests.java
+++ src/test/java/com/example/mybootapp3/Mybootapp3ApplicationTests.java
@@ -2,8 +2,8 @@ package com.example.mybootapp3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import jp.te4a.spring.boot.myapp5.mybootapp5.Mybootapp3Application;
+@SpringBootTest(classes = Mybootapp3Application.class)
 class Mybootapp3ApplicationTests {
 
 	@Test
